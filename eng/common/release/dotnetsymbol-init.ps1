a86a079c3605421838e21cae7c527bba26f7c464
Cesar Soares Lucas <disoares@microsoft.com>
2019-06-10 18:02:08 -0700

    Onboard in YAML publishing pipelines

---
+++ eng/common/release/dotnetsymbol-init.ps1
@@ -0,0 +1,28 @@
+param (
+  $dotnetsymbolVersion = $null
+)
+
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version 2.0
+
+. $PSScriptRoot\..\tools.ps1
+
+$verbosity = "minimal"
+
+function Installdotnetsymbol ($dotnetsymbolVersion) {
+  $dotnetsymbolPackageName = "dotnet-symbol"
+
+  $dotnetRoot = InitializeDotNetCli -install:$true
+  $dotnet = "$dotnetRoot\dotnet.exe"
+  $toolList = & "$dotnet" tool list --global
+
+  if ($toolList -like "*$dotnetsymbolPackageName*") {
+    & "$dotnet" tool uninstall $dotnetsymbolPackageName --global
+  }
+ 
+  Write-Host "Installing dotnet-symbol version $dotnetsymbolVersion..."
+  Write-Host "You may need to restart your command window if this is the first dotnet tool you have installed."
+  & "$dotnet" tool install $dotnetsymbolPackageName --version $dotnetsymbolVersion --verbosity $verbosity --global
+}
+
+Installdotnetsymbol $dotnetsymbolVersion
---
+++ eng/common/release/sourcelink-cli-init.ps1
@@ -0,0 +1,28 @@
+param (
+  $sourcelinkCliVersion = $null
+)
+
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version 2.0
+
+. $PSScriptRoot\..\tools.ps1
+
+$verbosity = "minimal"
+
+function InstallSourcelinkCli ($sourcelinkCliVersion) {
+  $sourcelinkCliPackageName = "sourcelink"
+
+  $dotnetRoot = InitializeDotNetCli -install:$true
+  $dotnet = "$dotnetRoot\dotnet.exe"
+  $toolList = & "$dotnet" tool list --global
+
+  if ($toolList -like "*$sourcelinkCliPackageName*") {
+    & "$dotnet" tool uninstall $sourcelinkCliPackageName --global
+  }
+ 
+  Write-Host "Installing SourceLink CLI version $sourcelinkCliVersion..."
+  Write-Host "You may need to restart your command window if this is the first dotnet tool you have installed."
+  & "$dotnet" tool install $sourcelinkCliPackageName --version $sourcelinkCliVersion --verbosity $verbosity --global
+}
+
+InstallSourcelinkCli $sourcelinkCliVersion
